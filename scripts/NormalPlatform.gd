855f7a09fe55627e4979b5466bd0e39d1226980b
Guilherme Oliveira <htoguilherme@gmail.com>
2018-03-06 04:16:02 -0300

    Normal platforms

---
+++ scripts/NormalPlatform.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	connect("body_entered", self, "_on_body_entered")
+	pass
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		if body.position.y < position.y:
+			body.jump()
