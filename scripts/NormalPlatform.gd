bff9b52556fff1d2c4de17fd61aa88b88224c381
Guilherme Oliveira <htoguilherme@gmail.com>
2018-03-07 01:01:19 -0300

    Spawning initial platforms

--- scripts/NormalPlatform.gd
+++ scripts/NormalPlatform.gd
@@ -1,12 +1,11 @@
 extends Area2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+onready var sprite = $Sprite
+var sprite_half_width
 
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
-	pass
+	sprite_half_width = sprite.texture.get_size().x / 2 * scale.x
 
 func _on_body_entered(body):
 	if body.name == "Player":
